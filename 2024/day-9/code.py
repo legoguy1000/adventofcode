c2e1ffd9052f6915b41d40b07bdfca01cb705240
Alex Resnick <adr8292@gmail.com>
2024-12-10 13:17:46 -0600

    its something stupid

--- 2024/day-9/code.py
+++ 2024/day-9/code.py
@@ -1,5 +1,6 @@
 import os
 
+
 def getlast(arr: list) -> int:
     i = len(arr) - 1
     for item in arr[::-1]:
@@ -8,6 +9,7 @@ def getlast(arr: list) -> int:
         else:
             i -= 1
 
+
 def part1():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     total = 0
@@ -15,22 +17,22 @@ def part1():
         line = f.readline()
         arr = []
         id = 0
-        for i in range(0,len(line)):
+        for i in range(0, len(line)):
             if i % 2 == 0:
-                arr.extend([str(id)]*int(line[i]))
+                arr.extend([str(id)] * int(line[i]))
                 id += 1
             else:
-                arr.extend(["."]*int(line[i]))
+                arr.extend(["."] * int(line[i]))
         print(arr)
-        last = len(arr)
-        for i in range(0,len(arr)):
-            if arr[i] == ".":
-                if i >= last:
-                    print(arr)
+        last = len(arr) - 1
+        for i in range(0, len(arr)):
+            for j in range(last, 0, -1):
+                if arr[i] == "." and arr[j] != ".":
+                    arr[i], arr[j] = arr[j], "."
+                    last = j
                     break
-                last = getlast(arr)
-                arr[i], arr[last] = arr[last], "."
-        total = sum([int(arr[i]) * i for i in range(0,len(arr)) if arr[i] != "."])
+        valid = [x for x in arr if x != "."]
+        total = sum([int(valid[k]) * k for k in range(0, len(valid))])
 
     print(f"Part1: The Sum is {total}")
 
@@ -41,7 +43,7 @@ def part2():
     sum = 0
     with open(dir_path + "/input.txt", encoding="utf-8") as f:
         line = f.readline()
-        
+
     print(f"Part 2: The Sum is {sum}")
 
 
